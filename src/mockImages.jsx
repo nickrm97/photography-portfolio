1e5510e16bd879a4d8a92d3e8037b1a9347c85c2
nickrm97 <nicholasrmills@icloud.com>
2020-03-11 22:49:03 +1100

    test uploading images

--- src/mockImages.jsx
+++ src/mockImages.jsx
@@ -1,4 +1,5 @@
-const urlPath = "./pics/";
+const urlPath =
+  "https://s3-ap-southeast-2.amazonaws.com/nicholasmills.me/photos/";
 
 export const getURL = url => {
   return urlPath + url;
@@ -8,17 +9,37 @@ const images = [
   {
     location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("lights.jpg")
+    url: getURL("iceland/mountain.JPG")
   },
   {
     location: "Westfjords, Iceland",
-    caption: "Test",
-    url: getURL("blacksand.jpg")
+    caption: "",
+    url: getURL("barn.JPG")
   },
   {
-    location: "Canal",
+    location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("lights.jpg")
+    url: getURL("house.JPG")
+  },
+  {
+    location: "Westfjords, Iceland",
+    caption: "",
+    url: getURL("north1.JPG")
+  },
+  {
+    location: "Westfjords, Iceland",
+    caption: "",
+    url: getURL("north2.JPG")
+  },
+  {
+    location: "Westfjords, Iceland",
+    caption: "",
+    url: getURL("north3.JPG")
+  },
+  {
+    location: "Westfjords, Iceland",
+    caption: "",
+    url: getURL("north4.JPG")
   }
 ];
 

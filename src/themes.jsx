ec45a4030149005f4da64b6a1998b7baf723ae7c
nickrm97 <nicholasrmills@icloud.com>
2020-03-13 09:23:06 +1100

    Updating theme descriptions

--- src/themes.jsx
+++ src/themes.jsx
@@ -5,7 +5,7 @@ export const themes = [
     id: "iceland",
     title: "Westfjords, Iceland",
     description:
-      "Bright green skies, abandoned houses and mirror-like lakes. A collection of photos taken out the car window, lying in the snow and in remote villages. All photos taken during an impromptu roadtrip to the Westfjords, a region in the east of Iceland.",
+      "Bright green skies, abandoned houses and mirror-like lakes. A collection of photos taken out the car window, lying in the snow and in remote villages. Memories from an impromptu roadtrip to the Westfjords, a region in the east of Iceland.",
     colours: ["#eeeef0", "#cfdfe9", "#c59748", "#1eb186", "#1b1b1b"],
     images: icelandImages
   },
@@ -13,7 +13,7 @@ export const themes = [
     id: "usa",
     title: "United States and Cuba",
     description:
-      "Six weeks in the United States; a country full of unfounded confidence, fast food and materialism. A warm and joyous intermission in Havana was relished. Images of cityscapes, friends and the friendly streets.",
+      "Six weeks through the United States; a country full of fast food, independence and diverse subcultures. A brief escape from New York's unforgiving winter through a Havana sidetrip was relished. One Portra 400 35mm roll of cityscapes, mates and golden days.",
     colours: ["#e8e3e4", "#7d8e49", "#e7ae59", "#972928", "#45858e"],
     images: usaImages
   }

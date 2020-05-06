f4398ffdaace3a042da8a5521295ff54fbcdbb87
nickrm97 <nicholasrmills@icloud.com>
2020-05-06 19:41:41 +1000

    Adding isolation album

--- src/images.jsx
+++ src/images.jsx
@@ -1,76 +1,119 @@
 const urlPath =
   "https://s3-ap-southeast-2.amazonaws.com/nicholasmills.me/photos/";
 
-export const getURL = url => {
+export const getURL = (url) => {
   return urlPath + url;
 };
+
+export const isolationImages = [
+  {
+    location: "Reynisfjara Beach, Iceland",
+    caption: "",
+    url: getURL("isolation/ocean.JPG"),
+  },
+  {
+    location: "Dandenong Ranges, Australia",
+    caption: "",
+    url: getURL("isolation/road.JPG"),
+  },
+  {
+    location: "Dandenong Ranges, Australia",
+    caption: "",
+    url: getURL("isolation/treesky.JPG"),
+  },
+  {
+    location: "Westfjords, Iceland",
+    caption: "",
+    url: getURL("isolation/farm.JPG"),
+  },
+  {
+    location: "Dandenong Ranges, Australia",
+    caption: "",
+    url: getURL("isolation/dalesup.JPG"),
+  },
+  {
+    location: "Reykjavik, Iceland",
+    caption: "",
+    url: getURL("isolation/across1.JPG"),
+  },
+  {
+    location: "Reykjavik, Iceland",
+    caption: "",
+    url: getURL("isolation/across2.JPG"),
+  },
+  {
+    location: "Lockdown",
+    caption: "",
+    url: getURL("isolation/across3.JPG"),
+  },
+];
 
 export const icelandImages = [
   {
     location: "Vesturbyggð, Iceland",
     caption: "",
-    url: getURL("iceland/barn.JPG")
+    url: getURL("iceland/barn.JPG"),
   },
   {
     location: "Vesturbyggð, Iceland",
     caption: "",
-    url: getURL("iceland/house.JPG")
+    url: getURL("iceland/house.JPG"),
   },
   {
     location: "Vesturbyggð, Iceland",
     caption: "",
-    url: getURL("iceland/mountain.JPG")
+    url: getURL("iceland/mountain.JPG"),
   },
   {
     location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("iceland/north1.JPG")
+    url: getURL("iceland/north1.JPG"),
   },
   {
     location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("iceland/north2.JPG")
+    url: getURL("iceland/north2.JPG"),
   },
   {
     location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("iceland/north3.JPG")
+    url: getURL("iceland/north3.JPG"),
   },
   {
     location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("iceland/north4.JPG")
+    url: getURL("iceland/north4.JPG"),
   },
   {
     location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("iceland/lake.JPG")
+    url: getURL("iceland/lake.JPG"),
   },
   {
     location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("iceland/gradient.JPG")
+    url: getURL("iceland/gradient.JPG"),
   },
   {
     location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("iceland/lake2.JPG")
+    url: getURL("iceland/lake2.JPG"),
   },
   {
     location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("iceland/lake3.JPG")
+    url: getURL("iceland/lake3.JPG"),
   },
   {
     location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("iceland/winding.JPG")
+    url: getURL("iceland/winding.JPG"),
   },
   {
     location: "Westfjords, Iceland",
     caption: "",
-    url: getURL("iceland/window.JPG")
-  }
+    url: getURL("iceland/window.JPG"),
+  },
 ];
 
 export const usaImages = [
@@ -78,7 +121,7 @@ export const usaImages = [
   {
     location: "East Bridgewater, MA",
     caption: "",
-    url: getURL("usa/meow.JPG")
+    url: getURL("usa/meow.JPG"),
   },
   { location: "Boston, MA", caption: "", url: getURL("usa/queens.JPG") },
   { location: "Brooklyn, NY", caption: "", url: getURL("usa/brooklyn.JPG") },
@@ -89,48 +132,48 @@ export const usaImages = [
   { location: "Los Angeles, CA", caption: "", url: getURL("usa/la.JPG") },
 
   { location: "Los Angeles, CA", caption: "", url: getURL("usa/outside.JPG") },
-  { location: "Los Angeles, CA", caption: "", url: getURL("usa/window.JPG") }
+  { location: "Los Angeles, CA", caption: "", url: getURL("usa/window.JPG") },
 ];
 
 export const japanImages = [
   {
     location: "Shinjuku",
     caption: "",
-    url: getURL("japan/lights.JPG")
+    url: getURL("japan/lights.JPG"),
   },
   {
     location: "Hanshin",
     caption: "",
-    url: getURL("japan/baseball.JPG")
+    url: getURL("japan/baseball.JPG"),
   },
   {
     location: "Travelling through Tokyo",
     caption: "",
-    url: getURL("japan/train2.JPG")
+    url: getURL("japan/train2.JPG"),
   },
   {
     location: "Shibuya",
     caption: "",
-    url: getURL("japan/sushi.JPG")
+    url: getURL("japan/sushi.JPG"),
   },
   {
     location: "Shinjuku",
     caption: "",
-    url: getURL("japan/police.JPG")
+    url: getURL("japan/police.JPG"),
   },
   {
     location: "Travelling through Tokyo",
     caption: "",
-    url: getURL("japan/insidetrain.JPG")
+    url: getURL("japan/insidetrain.JPG"),
   },
   {
     location: "Shinjuku",
     caption: "",
-    url: getURL("japan/yodo.JPG")
+    url: getURL("japan/yodo.JPG"),
   },
   {
     location: "Tokyo Station",
     caption: "",
-    url: getURL("japan/train.JPG")
-  }
+    url: getURL("japan/train.JPG"),
+  },
 ];
--- src/themes.jsx
+++ src/themes.jsx
@@ -1,13 +1,26 @@
-import { icelandImages, usaImages, japanImages } from "./images";
+import {
+  isolationImages,
+  icelandImages,
+  usaImages,
+  japanImages,
+} from "./images";
 
 export const themes = [
+  {
+    id: "isolation",
+    title: "Life without the Bridge Hotel",
+    description:
+      "Quiet roads. Empty landscapes. Slowness of time during ~these uncertain times~.",
+    colours: ["#dcd7d6", "#c3c6ce", "#b2c8d0", "#515f6c", "#375a42"],
+    images: isolationImages,
+  },
   {
     id: "iceland",
     title: "Westfjords, Iceland",
     description:
       "Bright green skies, abandoned houses and mirror-like lakes. A collection of photos taken out the car window, lying in the snow and in remote villages. Memories from an impromptu roadtrip to the Westfjords, a region in the west of Iceland.",
     colours: ["#eeeef0", "#cfdfe9", "#c59748", "#1eb186", "#1b1b1b"],
-    images: icelandImages
+    images: icelandImages,
   },
   {
     id: "usa",
@@ -15,7 +28,7 @@ export const themes = [
     description:
       "Six weeks through the United States; a country full of fast food, independence and diverse subcultures. A brief escape from New York's unforgiving winter through a Havana sidetrip was relished. One Portra 400 35mm roll of cityscapes, mates and golden days.",
     colours: ["#e8e3e4", "#7d8e49", "#e7ae59", "#972928", "#45858e"],
-    images: usaImages
+    images: usaImages,
   },
   {
     id: "japan",
@@ -23,6 +36,6 @@ export const themes = [
     description:
       "From baseball in Ōsaka-Kōbe to hiding tatoos in Shibuya, I became obsessed with the streets of Japan. This was my first holiday shooting film, using my Olympus Trip35 with a mix of Portra and Ultramax 35mm.",
     colours: ["#edee55", "#0a84c8", "#cf5138", "#59984d", "#110605"],
-    images: japanImages
-  }
+    images: japanImages,
+  },
 ];
